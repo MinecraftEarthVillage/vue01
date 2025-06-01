906852bdbaf2399c168846c587f35472678e3ff2
战虎 <earthvillage.top@outlook.com>
2025-06-01 22:51:46 +0800

    。

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue01/'  // ⚠️ 必须与你的仓库名称完全匹配（包括大小写）
+    : '/'
 })
